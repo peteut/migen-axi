ea68adfab106384baaf4a8bfb80d819fe2f03488
Alain Péteut <alain.peteut@spacetek.ch>
2022-12-29 11:45:59 +0000

    *: migrate to pyproject.toml, bump deps.
    * update CI test to use Python 3.9.
    
    * fixes #29.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    setup_requires=('pbr>=1.10', 'setuptools>=25.1'),
-    pbr=True,
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
     packages=find_packages(where="src"),
     package_dir={"": "src"})
