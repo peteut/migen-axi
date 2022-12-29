00b6807dd1b29049b815b8ab43d79b5fc5d75bd5
Alain Péteut <alain.peteut@spacetek.ch>
2022-12-29 13:18:13 +0000

    pyproject.toml: cleanup, fix typos.

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    setup_requires=["setuptools_scm"],
-    use_scm_version=True,
-    packages=find_packages(where="src"),
-    package_dir={"": "src"})
